d119ce9c534a10fd83d7dd60c9c7c36a2969f55d
John Ouellet <labboy0276@yahoo.com>
2017-12-14 09:06:36 -0500

    Add in tweaks, bs changes + static templates

--- inc/timber.php
+++ inc/timber.php
@@ -23,9 +23,11 @@ Timber::$dirname = array('templates', 'views');
 class StarterSite extends TimberSite {
 
   function __construct() {
+    add_theme_support( 'title-tag' );
     add_theme_support( 'post-formats' );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'menus' );
+    add_theme_support( 'custom-logo' );
     add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
     add_filter( 'timber_context', array( $this, 'add_to_context' ) );
     // add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
